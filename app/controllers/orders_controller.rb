1ff368d68a8d8a9cc401e30fed0e1c5a3ee09bec
Brais Fernandez <braisoncrece@gmail.com>
2022-09-07 18:09:28 +0200

    added React functionality to SelectPayType

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -87,6 +87,18 @@ class OrdersController < ApplicationController
       params.require(:order).permit(:name, :address, :email, :pay_type)
     end
 
+    def pay_type_params
+      if order_params[:pay_type] == 'Credit card'
+        params.require(:order).permit(:credit_card_number, :expiration_date)
+      elsif order_params[:pay_type] == 'Check'
+        params.require(:order).permit(:routing_number, :account_number)        
+      elsif order_params[:pay_type] == 'Purchase order'
+        params.require(:order).permit(:po_number)
+      else
+        {}
+      end
+    end
+
     def ensure_cart_isnt_empty
       if @cart.line_items.empty?
         redirect_to store_index_url, notice: 'Your cart is empty'
